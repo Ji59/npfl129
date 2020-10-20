77c25d0b87e88f18c7da681fe8da24ecbd1e56f6
ji59 <jirikotal@vivaldi.net>
2020-10-20 11:41:00 +0200

    Completed first labs assignment.

--- labs/01/linear_regression_features.py
+++ labs/01/linear_regression_features.py
@@ -12,35 +12,46 @@ parser.add_argument("--data_size", default=40, type=int, help="Data size")
 parser.add_argument("--range", default=3, type=int, help="Feature order range")
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
-parser.add_argument("--test_size", default=0.5, type=lambda x:int(x) if x.isdigit() else float(x), help="Test set size")
+parser.add_argument("--test_size", default=0.5, type=lambda x: int(x) if x.isdigit() else float(x),
+										help="Test set size")
+
+
 # If you add more arguments, ReCodEx will keep them with your default values.
 
 def main(args):
-    # Create the data
-    xs = np.linspace(0, 7, num=args.data_size)
-    ys = np.sin(xs) + np.random.RandomState(args.seed).normal(0, 0.2, size=args.data_size)
+	# Create the data
+	xs = np.linspace(0, 7, num=args.data_size)
+	ys = np.sin(xs) + np.random.RandomState(args.seed).normal(0, 0.2, size=args.data_size)
 
-    rmses = []
-    for order in range(1, args.range + 1):
-        # TODO: Create features of x^1, ..., x^order.
+	rmses = []
+	for order in range(1, args.range + 1):
+		# TODO: Create features of x^1, ..., x^order.
+		x = xs.reshape(len(xs), 1)
+		for i in range(2, order + 1):
+			x = np.c_[x, np.power(x[:, 0], i)]
 
-        # TODO: Split the data into a train set and a test set.
-        # Use `sklearn.model_selection.train_test_split` method call, passing
-        # arguments `test_size=args.test_size, random_state=args.seed`.
+		# TODO: Split the data into a train set and a test set.
+		# Use `sklearn.model_selection.train_test_split` method call, passing
+		# arguments `test_size=args.test_size, random_state=args.seed`.
+		x_train, x_test, y_train, y_test = \
+			sklearn.model_selection.train_test_split(x, ys, test_size=args.test_size, random_state=args.seed)
 
-        # TODO: Fit a linear regression model using `sklearn.linear_model.LinearRegression`.
+		# TODO: Fit a linear regression model using `sklearn.linear_model.LinearRegression`.
+		model = sklearn.linear_model.LinearRegression().fit(x_train, y_train)
 
-        # TODO: Predict targets on the test set using the trained model.
+		# TODO: Predict targets on the test set using the trained model.
+		y_predicted = model.predict(x_test)
 
-        # TODO: Compute root mean square error on the test set predictions
-        rmse = None
+		# TODO: Compute root mean square error on the test set predictions
+		rmse = np.sqrt(np.sum(np.power(y_test - y_predicted, 2)) / len(y_test))
 
-        rmses.append(rmse)
+		rmses.append(rmse)
 
-    return rmses
+	return rmses
+
 
 if __name__ == "__main__":
-    args = parser.parse_args([] if "__file__" not in globals() else None)
-    rmses = main(args)
-    for order, rmse in enumerate(rmses):
-        print("Maximum feature order {}: {:.2f} RMSE".format(order + 1, rmse))
+	args = parser.parse_args([] if "__file__" not in globals() else None)
+	rmses = main(args)
+	for order, rmse in enumerate(rmses):
+		print("Maximum feature order {}: {:.2f} RMSE".format(order + 1, rmse))
--- labs/01/linear_regression_manual.py
+++ labs/01/linear_regression_manual.py
@@ -10,35 +10,43 @@ parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
-parser.add_argument("--test_size", default=0.1, type=lambda x:int(x) if x.isdigit() else float(x), help="Test set size")
+parser.add_argument("--test_size", default=0.1, type=lambda x: int(x) if x.isdigit() else float(x),
+										help="Test set size")
+
+
 # If you add more arguments, ReCodEx will keep them with your default values.
 
 def main(args):
-    # Load Boston housing dataset
-    dataset = sklearn.datasets.load_boston()
+	# Load Boston housing dataset
+	dataset = sklearn.datasets.load_boston()
 
-    # The input data are in dataset.data, targets are in dataset.target.
+	# The input data are in dataset.data, targets are in dataset.target.
 
-    # If you want to learn about the dataset, uncomment the following line.
-    # print(dataset.DESCR)
+	# If you want to learn about the dataset, uncomment the following line.
+	# print(dataset.DESCR)
 
-    # TODO: Append a new feature to all input data, with value "1"
+	# TODO: Append a new feature to all input data, with value "1"
+	X = np.c_[dataset.data, np.ones((len(dataset.data), 1))]
 
-    # TODO: Split the dataset into a train set and a test set.
-    # Use `sklearn.model_selection.train_test_split` method call, passing
-    # arguments `test_size=args.test_size, random_state=args.seed`.
+	# TODO: Split the dataset into a train set and a test set.
+	# Use `sklearn.model_selection.train_test_split` method call, passing
+	# arguments `test_size=args.test_size, random_state=args.seed`.
+	X_train, X_test, t_train, t_test = \
+		sklearn.model_selection.train_test_split(X, dataset.target, test_size=args.test_size, random_state=args.seed)
+	# TODO: Solve the linear regression using the algorithm from the lecture,
+	# explicitly computing the matrix inverse (using `np.linalg.inv`).
 
-    # TODO: Solve the linear regression using the algorithm from the lecture,
-    # explicitly computing the matrix inverse (using `np.linalg.inv`).
+	w = np.linalg.inv(np.transpose(X_train) @ X_train) @ np.transpose(X_train) @ t_train
 
-    # TODO: Predict target values on the test set
+	# TODO: Compute root mean square error on the test set predictions
+	rmse = np.sqrt(np.sum(np.power(X_test @ w - t_test, 2)) / len(t_test))
 
-    # TODO: Compute root mean square error on the test set predictions
-    rmse = None
+	return rmse
 
-    return rmse
+
+# TODO: Predict target values on the test set
 
 if __name__ == "__main__":
-    args = parser.parse_args([] if "__file__" not in globals() else None)
-    rmse = main(args)
-    print("{:.2f}".format(rmse))
+	args = parser.parse_args([] if "__file__" not in globals() else None)
+	rmse = main(args)
+	print("{:.2f}".format(rmse))
