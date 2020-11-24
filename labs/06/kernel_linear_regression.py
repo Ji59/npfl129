aa2789315ece7dcf45c1b97107fa910e9ee1b00c
ji59 <jirikotal@vivaldi.net>
2020-11-24 16:41:23 +0100

    Completed 06/kernel_linear_regression

--- labs/06/kernel_linear_regression.py
+++ labs/06/kernel_linear_regression.py
@@ -19,68 +19,151 @@ parser.add_argument("--learning_rate", default=0.01, type=float, help="Learning 
 parser.add_argument("--plot", default=False, const=True, nargs="?", type=str, help="Plot the predictions")
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
+
+
 # If you add more arguments, ReCodEx will keep them with your default values.
 
+
+deg = 0;
+gamma = 1;
+gaussian = True;
+
+
+def get_poly_kernel_xy(x, y):
+	temp = gamma * (x @ y) + 1;
+	out = 1;
+	for i in range(deg):
+		out *= temp;
+	return out;
+
+
+def get_gaussian_kernel_xy(x, y):
+	diff = x - y;
+	return np.exp(-gamma * diff @ diff);
+
+
+def get_kernel(data0, data1):
+	same = np.all(data0 == data1)
+	if (len(data0.shape) < 2):
+		data0 = data0.reshape(-1, 1)
+	if (len(data1.shape) < 2):
+		data1 = data1.reshape(-1, 1)
+	train_kernel = np.zeros((data0.shape[0], data1.shape[0]));
+	for i in range(len(data0)):
+		x = np.array(data0[i]);
+		if same:
+			for j in range(i, len(data1)):
+				y = np.array(data1[j]);
+				train_kernel[j, i] = train_kernel[i, j] = (get_gaussian_kernel_xy(x, y) if (gaussian) else get_poly_kernel_xy(x, y));
+		else:
+			for j in range(len(data1)):
+				y = np.array(data1[j]);
+				train_kernel[i, j] = (get_gaussian_kernel_xy(x, y) if (gaussian) else get_poly_kernel_xy(x, y));
+	return train_kernel;
+
+
+def get_rmse(kernel, target, beta, bias):
+	mse = 0;
+	for i in range(len(target)):
+		error = target[i] - np.sum(kernel[i] @ beta) - bias;
+		mse += error * error;
+	return np.sqrt(mse / len(target));
+
+
 def main(args):
-    # Create a random generator with a given seed
-    generator = np.random.RandomState(args.seed)
+	# Create a random generator with a given seed
+	generator = np.random.RandomState(args.seed)
 
-    # Generate an artifical regression dataset
-    train_data = np.linspace(-1, 1, args.data_size)
-    train_targets = np.sin(5 * train_data) + generator.normal(scale=0.25, size=args.data_size) + 1
+	# Generate an artifical regression dataset
+	train_data = np.linspace(-1, 1, args.data_size)
+	train_targets = np.sin(5 * train_data) + generator.normal(scale=0.25, size=args.data_size) + 1
 
-    test_data = np.linspace(-1.2, 1.2, 2 * args.data_size)
-    test_targets = np.sin(5 * test_data) + 1
+	test_data = np.linspace(-1.2, 1.2, 2 * args.data_size)
+	test_targets = np.sin(5 * test_data) + 1
 
-    betas = np.zeros(args.data_size)
+	betas = np.zeros(args.data_size)
 
-    # TODO: Perform `args.iterations` of SGD-like updates, but in dual formulation
-    # using `betas` as weights of individual training examples.
-    #
-    # We assume the primary formulation of our model is
-    #   y = phi(x)^T w + bias
-    # and the loss in the primary problem is batched MSE with L2 regularization:
-    #   L = sum_{i \in B} 1/|B| * [1/2 * (phi(x_i)^T w + bias - target_i)^2] + 1/2 * args.l2 * w^2
-    #
-    # For `bias`, use explicitly the average of the training targets, and do
-    # not update it futher during training.
-    #
-    # Instead of using feature map `phi` directly, we use a given kernel computing
-    #   K(x, y) = phi(x)^T phi(y)
-    # We consider the following `args.kernel`s:
-    # - "poly": K(x, y; degree, gamma) = (gamma * x^T y + 1) ^ degree
-    # - "rbf": K(x, y; gamma) = exp^{- gamma * ||x - y||^2}
-    #
-    # After each iteration, compute RMSE both on training and testing data.
-    train_rmses, test_rmses = [], []
+	batch_size = args.batch_size;
+	betas_temp = np.zeros(batch_size);
 
-    for iteration in range(args.iterations):
-        permutation = generator.permutation(train_data.shape[0])
+	# TODO: Perform `args.iterations` of SGD-like updates, but in dual formulation
+	# using `betas` as weights of individual training examples.
+	#
+	# We assume the primary formulation of our model is
+	#   y = phi(x)^T w + bias
+	# and the loss in the primary problem is batched MSE with L2 regularization:
+	#   L = sum_{i \in B} 1/|B| * [1/2 * (phi(x_i)^T w + bias - target_i)^2] + 1/2 * args.l2 * w^2
+	#
+	# For `bias`, use explicitly the average of the training targets, and do
+	# not update it futher during training.
+	#
+	# Instead of using feature map `phi` directly, we use a given kernel computing
+	#   K(x, y) = phi(x)^T phi(y)
+	# We consider the following `args.kernel`s:
+	# - "poly": K(x, y; degree, gamma) = (gamma * x^T y + 1) ^ degree
+	# - "rbf": K(x, y; gamma) = exp^{- gamma * ||x - y||^2}
+	#
+	# After each iteration, compute RMSE both on training and testing data.
+	train_rmses, test_rmses = [], []
 
-        # TODO: Process the data in the order of `permutation`, performing
-        # batched updates to the `betas`. You can assume that `args.batch_size`
-        # exactly divides `train_data.shape[0]`.
+	if (args.kernel) == "poly":
+		print("poly")
+		global gaussian;
+		gaussian = False;
+		global deg;
+		deg = args.kernel_degree;
+	global gamma;
+	gamma = args.kernel_gamma;
 
-        # TODO: Append RMSE on training and testing data to `train_rmses` and
-        # `test_rmses` after the iteration.
+	train_kernel = get_kernel(train_data, train_data);
+	test_kernel = get_kernel(test_data, train_data);
 
-        if (iteration + 1) % 10 == 0:
-            print("Iteration {}, train RMSE {:.2f}, test RMSE {:.2f}".format(
-                iteration + 1, train_rmses[-1], test_rmses[-1]))
+	bias = np.average(train_targets);
 
-    if args.plot:
-        # If you want the plotting to work (not required for ReCodEx), compute the `test_predictions`.
-        test_predictions = None
+	for iteration in range(args.iterations):
+		permutation = generator.permutation(train_data.shape[0])
 
-        plt.plot(train_data, train_targets, "bo", label="Train targets")
-        plt.plot(test_data, test_targets, "ro", label="Test targets")
-        plt.plot(test_data, test_predictions, "g-", label="Predictions")
-        plt.legend()
-        if args.plot is True: plt.show()
-        else: plt.savefig(args.plot, transparent=True, bbox_inches="tight")
+		# TODO: Process the data in the order of `permutation`, performing
+		# batched updates to the `betas`. You can assume that `args.batch_size`
+		# exactly divides `train_data.shape[0]`.
 
-    return train_rmses, test_rmses
+		for i in range(len(permutation)):
+			j = i % batch_size;
+			c = permutation[i]
+			betas_temp[j] = args.learning_rate * (train_targets[c] - train_kernel[c] @ betas - bias) / batch_size;
+
+			if (j + 1 == batch_size):
+				betas = (1 - args.learning_rate * args.l2) * betas;
+				for k in range(batch_size):
+					betas[permutation[i - k]] += betas_temp[-k - 1];
+
+
+		# TODO: Append RMSE on training and testing data to `train_rmses` and
+		# `test_rmses` after the iteration.
+
+		train_rmses.append(get_rmse(train_kernel, train_targets, betas, bias));
+		test_rmses.append(get_rmse(test_kernel, test_targets, betas, bias));
+
+		if (iteration + 1) % 10 == 0:
+			print("Iteration {}, train RMSE {:.2f}, test RMSE {:.2f}".format(
+				iteration + 1, train_rmses[-1], test_rmses[-1]))
+
+	if args.plot:
+		# If you want the plotting to work (not required for ReCodEx), compute the `test_predictions`.
+		test_predictions = test_kernel @ betas + bias
+
+		plt.plot(train_data, train_targets, "bo", label="Train targets")
+		plt.plot(test_data, test_targets, "ro", label="Test targets")
+		plt.plot(test_data, test_predictions, "g-", label="Predictions")
+		plt.legend()
+		if args.plot is True:
+			plt.show()
+		else:
+			plt.savefig(args.plot, transparent=True, bbox_inches="tight")
+
+	return train_rmses, test_rmses
+
 
 if __name__ == "__main__":
-    args = parser.parse_args([] if "__file__" not in globals() else None)
-    main(args)
+	args = parser.parse_args([] if "__file__" not in globals() else None)
+	main(args)
