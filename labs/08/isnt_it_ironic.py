00c09c9976635583558f272b1580eb7a74e5765c
ji59 <jirikotal@vivaldi.net>
2020-12-11 14:47:22 +0100

    Partially completed 08 assignment.

--- labs/08/isnt_it_ironic.py
+++ labs/08/isnt_it_ironic.py
@@ -8,26 +8,33 @@ import sys
 import zipfile
 
 import numpy as np
+import random
+from sklearn.naive_bayes import GaussianNB;
+
+
+KONSTANTA = 100000
+
 
 class Dataset:
-    def __init__(self,
-                 name="isnt_it_ironic.train.zip",
-                 url="https://ufal.mff.cuni.cz/~straka/courses/npfl129/2021/datasets/"):
-        if not os.path.exists(name):
-            print("Downloading dataset {}...".format(name), file=sys.stderr)
-            urllib.request.urlretrieve(url + name, filename=name)
+	def __init__(self,
+							 name="isnt_it_ironic.train.zip",
+							 url="https://ufal.mff.cuni.cz/~straka/courses/npfl129/2021/datasets/"):
+		if not os.path.exists(name):
+			print("Downloading dataset {}...".format(name), file=sys.stderr)
+			urllib.request.urlretrieve(url + name, filename=name)
 
-        # Load the dataset and split it into `data` and `target`.
-        self.data = []
-        self.target = []
+		# Load the dataset and split it into `data` and `target`.
+		self.data = []
+		self.target = []
 
-        with zipfile.ZipFile(name, "r") as dataset_file:
-            with dataset_file.open(name.replace(".zip", ".txt"), "r") as train_file:
-                for line in train_file:
-                    label, text = line.decode("utf-8").rstrip("\n").split("\t")
-                    self.data.append(text)
-                    self.target.append(int(label))
-        self.target = np.array(self.target, np.int32)
+		with zipfile.ZipFile(name, "r") as dataset_file:
+			with dataset_file.open(name.replace(".zip", ".txt"), "r") as train_file:
+				for line in train_file:
+					label, text = line.decode("utf-8").rstrip("\n").split("\t")
+					self.data.append(text)
+					self.target.append(int(label))
+		self.target = np.array(self.target, np.int32)
+
 
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
@@ -37,33 +44,78 @@ parser.add_argument("--seed", default=42, type=int, help="Random seed")
 # For these and any other arguments you add, ReCodEx will keep your default value.
 parser.add_argument("--model_path", default="isnt_it_ironic.model", type=str, help="Model path")
 
+
 def main(args):
-    if args.predict is None:
-        # We are training a model.
-        np.random.seed(args.seed)
-        train = Dataset()
+	if args.predict is None:
+		# We are training a model.
+		np.random.seed(args.seed)
+		train = Dataset()
 
-        # TODO: Train a model on the given dataset and store it in `model`.
-        model = None
+		words_deceptikon = {}
+		data = np.zeros(KONSTANTA);
+		n = 0;
+		k = 1;
+		ones = 0;
+		all = 0;
+		target = np.zeros(KONSTANTA);
+		for i in range(len(train.target)):
+			words = train.data[i].lower().split();
+			if (train.target[i] == 1):
+				ones += len(words);
+			all += len(words);
+			for word in words:
+				if (word not in words_deceptikon):
+					words_deceptikon[word] = k;
+					k += 1;
+				data[n] = words_deceptikon[word];
+				target[n] = train.target[i]
+				n += 1;
+				if (n > len(data)):
+					data = np.append(data, np.zeros(KONSTANTA));
+					target = np.append(target, np.zeros(KONSTANTA));
+		data = data[:n];
+		target = target[:n];
 
-        # Serialize the model.
-        with lzma.open(args.model_path, "wb") as model_file:
-            pickle.dump(model, model_file)
+		# TODO: Train a model on the given dataset and store it in `model`.
+		model = GaussianNB();
+		model.fit(data.reshape(-1, 1), target);
 
-    else:
-        # Use the model and return test set predictions.
-        test = Dataset(args.predict)
 
-        with lzma.open(args.model_path, "rb") as model_file:
-            model = pickle.load(model_file)
+		# Serialize the model.
+		with lzma.open(args.model_path, "wb") as model_file:
+			pickle.dump([words_deceptikon, model, ones, all], model_file)
 
-        # TODO: Generate `predictions` with the test set predictions, either
-        # as a Python list of a NumPy array.
-        predictions = None
+	else:
+		# Use the model and return test set predictions.
+		test = Dataset(args.predict)
 
-        return predictions
+		with lzma.open(args.model_path, "rb") as model_file:
+			file = pickle.load(model_file)
+			words = file[0]
+			model = file[1]
+			ones = file[2]
+			all = file[3]
+
+		# TODO: Generate `predictions` with the test set predictions, either
+		# as a Python list of a NumPy array.
+
+		predictions = np.zeros(len(test.target));
+
+		for i in range(len(test.data)):
+			sum = 0;
+			n = 0;
+			for word in test.data[i].lower().split():
+				if word in words:
+					sum += model.predict(np.array([words[word]]).reshape(-1, 1));
+				else:
+					sum += 1 if random.uniform(0, 1) >= ones / all else 0;
+				n += 1;
+			if sum > n / 2:
+				predictions[i] = 1;
+
+		return predictions
 
 
 if __name__ == "__main__":
-    args = parser.parse_args([] if "__file__" not in globals() else None)
-    main(args)
+	args = parser.parse_args([] if "__file__" not in globals() else None)
+	main(args)
--- labs/08/naive_bayes.py
+++ labs/08/naive_bayes.py
@@ -14,38 +14,101 @@ parser.add_argument("--naive_bayes_type", default="gaussian", type=str, help="NB
 parser.add_argument("--classes", default=10, type=int, help="Number of classes")
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
-parser.add_argument("--test_size", default=0.5, type=lambda x:int(x) if x.isdigit() else float(x), help="Test set size")
+parser.add_argument("--test_size", default=0.5, type=lambda x: int(x) if x.isdigit() else float(x), help="Test set size")
+
+
 # If you add more arguments, ReCodEx will keep them with your default values.
 
+
+def gaussian(frequency_table):
+	mean_var = {};
+	for t in frequency_table:
+		mean_var[t] = [];
+		for i in range(len(frequency_table[t][1])):
+			mean_var[t].append([0, 0]);
+			for x in frequency_table[t][1][i]:
+				mean_var[t][i][0] += frequency_table[t][1][i][x] * x;
+			mean_var[t][i][0] /= frequency_table[t][0];
+			mean = mean_var[t][i][0];
+			for x in frequency_table[t][1][i]:
+				diff = x - mean;
+				mean_var[t][i][1] += frequency_table[t][1][i][x] * diff * diff / frequency_table[t][0];
+			mean_var[t][i][1] = np.sqrt(mean_var[t][i][1] / frequency_table[t][0] + args.alpha);
+	return mean_var;
+
+
+def normal_distribution(x, mean, variance):
+	# return scipy.stats.norm.logpdf(x, loc=mean, scale=variance);
+	diff = x - mean
+	variance_ = np.exp(-diff * diff / (2 * variance * variance)) / (np.sqrt(2 * np.pi) * variance)
+	return np.log2(variance_)
+
+
 def main(args):
-    # Use the digits dataset.
-    data, target = sklearn.datasets.load_digits(n_class=args.classes, return_X_y=True)
+	# Use the digits dataset.
+	data, target = sklearn.datasets.load_digits(n_class=args.classes, return_X_y=True)
 
-    # Split the dataset into a train set and a test set.
-    train_data, test_data, train_target, test_target = sklearn.model_selection.train_test_split(
-        data, target, test_size=args.test_size, random_state=args.seed)
+	# Split the dataset into a train set and a test set.
+	train_data, test_data, train_target, test_target = sklearn.model_selection.train_test_split(
+		data, target, test_size=args.test_size, random_state=args.seed)
 
-    # TODO: Fit the naive Bayes classifier on the train data.
-    #
-    # The `args.naive_bayes_type` can be one of:
-    # - "gaussian": Fit Gaussian NB, by estimating mean and variance of the input
-    #   features. For variance estimation use
-    #     1/N * \sum_x (x - mean)^2
-    #   and additionally increase all estimated variances by `args.alpha`.
-    # - "multinomial": Multinomial NB with smoothing factor `args.alpha`
-    # - "bernoulli": Bernoulli NB with smoothing factor `args.alpha`
+	# TODO: Fit the naive Bayes classifier on the train data.
+	#
+	# The `args.naive_bayes_type` can be one of:
+	# - "gaussian": Fit Gaussian NB, by estimating mean and variance of the input
+	#   features. For variance estimation use
+	#     1/N * \sum_x (x - mean)^2
+	#   and additionally increase all estimated variances by `args.alpha`.
+	# - "multinomial": Multinomial NB with smoothing factor `args.alpha`
+	# - "bernoulli": Bernoulli NB with smoothing factor `args.alpha`
 
-    # TODO: Predict the test data classes and compute test accuracy.
-    #
-    # You can use `scipy.stats.norm` to compute probability density function
-    # of a Gaussian distribution -- it offers `pdf` and `logpdf` methods, among
-    # others.
-    test_accuracy = None
+	frequency_table = {}
+	targets = {}
 
-    return test_accuracy
+	for i in range(len(train_data)):
+		x = train_data[i];
+		t = train_target[i];
+		if t not in frequency_table:
+			frequency_table[t] = [0, []]
+		t_t = frequency_table[t]
+		t_t[0] += 1
+		for j in range(len(x)):
+			if len(t_t[1]) <= j:
+				frequency_table[t][1].append({});
+			if x[j] not in t_t[1][j]:
+				t_t[1][j][x[j]] = 0;
+			t_t[1][j][x[j]] += 1;
+
+	# TODO: Predict the test data classes and compute test accuracy.
+	#
+	# You can use `scipy.stats.norm` to compute probability density function
+	# of a Gaussian distribution -- it offers `pdf` and `logpdf` methods, among
+	# others.
+
+	test_accuracy = 0;
+
+	if args.naive_bayes_type == "gaussian":
+		mean_var = gaussian(frequency_table);
+		for i in range(len(test_data)):
+			x = test_data[i];
+			t = test_target[i];
+			k = 0;
+			maxim = -9999999999;
+			for c_k in mean_var:
+				probability = np.log(frequency_table[c_k][0] / len(train_target));
+				for i in range(len(mean_var[c_k])):
+					probability += normal_distribution(x[i], mean_var[c_k][i][0], mean_var[c_k][i][1]);
+				if probability > maxim:
+					maxim = probability;
+					k = c_k;
+			if t == k:
+				test_accuracy += 1;
+
+	return test_accuracy / len(test_target);
+
 
 if __name__ == "__main__":
-    args = parser.parse_args([] if "__file__" not in globals() else None)
-    test_accuracy = main(args)
+	args = parser.parse_args([] if "__file__" not in globals() else None)
+	test_accuracy = main(args)
 
-    print("Test accuracy {:.2f}%".format(100 * test_accuracy))
+	print("Test accuracy {:.2f}%".format(100 * test_accuracy))
